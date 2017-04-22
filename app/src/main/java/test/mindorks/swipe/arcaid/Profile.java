22815af3382d4af8d374a2db209540b451ab8a75
kameron90d <kameron90d@yahoo.com>
2017-04-22 10:50:55 -0500

    intial commit

---
+++ app/src/main/java/test/mindorks/swipe/arcaid/Profile.java
@@ -0,0 +1,58 @@
+package test.mindorks.swipe.arcaid;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by janisharali on 29/08/16.
+ */
+public class Profile {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("url")
+    @Expose
+    private String imageUrl;
+
+    @SerializedName("goal")
+    @Expose
+    private Integer goal;
+
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Integer goal) {
+        this.goal = goal;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/test/mindorks/swipe/arcaid/TinderCard.java
@@ -0,0 +1,84 @@
+package test.mindorks.swipe.arcaid;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.mindorks.placeholderview.annotations.Click;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
+import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
+import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
+
+/**
+ * Created by janisharali on 19/08/16.
+ */
+@Layout(R.layout.tinder_card_view)
+public class TinderCard {
+
+    @View(R.id.profileImageView)
+    private ImageView profileImageView;
+
+    @View(R.id.nameGoalTxt)
+    private TextView nameGoalTxt;
+
+    @View(R.id.descriptionNameTxt)
+    private TextView descriptionNameTxt;
+
+    private Profile mProfile;
+    private Context mContext;
+    private SwipePlaceHolderView mSwipeView;
+
+    public TinderCard(Context context, Profile profile, SwipePlaceHolderView swipeView) {
+        mContext = context;
+        mProfile = profile;
+        mSwipeView = swipeView;
+    }
+
+    @Resolve
+    private void onResolved(){
+        Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
+        nameGoalTxt.setText(mProfile.getName() + "\nGoal: $" + mProfile.getGoal());
+        descriptionNameTxt.setText(mProfile.getDescription());
+    }
+
+    @Click(R.id.profileImageView)
+    private void onClick(){
+        Log.d("EVENT", "profileImageView click");
+        mSwipeView.addView(this);
+    }
+
+    @SwipeOut
+    private void onSwipedOut(){
+        Log.d("EVENT", "onSwipedOut");
+        mSwipeView.addView(this);
+    }
+
+    @SwipeCancelState
+    private void onSwipeCancelState(){
+        Log.d("EVENT", "onSwipeCancelState");
+    }
+
+    @SwipeIn
+    private void onSwipeIn(){
+        Log.d("EVENT", "onSwipedIn");
+    }
+
+    @SwipeInState
+    private void onSwipeInState(){
+        Log.d("EVENT", "onSwipeInState");
+    }
+
+    @SwipeOutState
+    private void onSwipeOutState(){
+        Log.d("EVENT", "onSwipeOutState");
+    }
+}
