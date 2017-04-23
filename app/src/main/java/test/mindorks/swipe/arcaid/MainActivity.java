35b6addf88fcaf5892098a0c58db1aadf8a78c24
kameron90d <kameron90d@yahoo.com>
2017-04-22 21:25:04 -0500

    facebook login with tiles

--- app/src/main/java/test/mindorks/swipe/arcaid/MainActivity.java
+++ app/src/main/java/test/mindorks/swipe/arcaid/MainActivity.java
@@ -1,60 +1,62 @@
 package test.mindorks.swipe.arcaid;
 
-import android.content.Context;
-import android.graphics.Point;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
+import android.support.v7.view.ActionMode;
+import android.widget.TextView;
 
-import com.mindorks.placeholderview.SwipeDecor;
-import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SwipePlaceHolderView mSwipeView;
-    private Context mContext;
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-
-        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
-        mContext = getApplicationContext();
-
-        int bottomMargin = Utils.dpToPx(160);
-        Point windowSize = Utils.getDisplaySize(getWindowManager());
-        mSwipeView.getBuilder()
-                .setDisplayViewCount(3)
-                .setHeightSwipeDistFactor(10)
-                .setWidthSwipeDistFactor(5)
-                .setSwipeDecor(new SwipeDecor()
-                        .setViewWidth(windowSize.x)
-                        .setViewHeight(windowSize.y - bottomMargin)
-                        .setViewGravity(Gravity.TOP)
-                        .setPaddingTop(20)
-                        .setRelativeScale(0.01f)
-                .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
-                .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
-
-
-        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
-            mSwipeView.addView(new Card(mContext, profile, mSwipeView));
-        }
-
-        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+        loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+        textView = (TextView)findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                mSwipeView.doSwipe(false);
+
+            public void onSuccess(LoginResult loginResult) {
+
+                textView.setText("Login Success \n" +
+                        loginResult.getAccessToken().getUserId() +
+                        "\n"+loginResult.getAccessToken().getToken());
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
             }
         });
+    }
 
-        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSwipeView.doSwipe(true);
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Intent startNewActivity = new Intent(this, SwipeActivity.class);
+        startActivity(startNewActivity);
     }
 }
